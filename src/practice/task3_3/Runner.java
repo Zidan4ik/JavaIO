f4233c07f6e1a80802aed99eee73f7d98252b5b8
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-03-08 20:44:03 +0200

    task 3.3 (working with reading and handling info in file)

---
+++ src/practice/task3_3/Runner.java
@@ -0,0 +1,33 @@
+package practice.task3_3;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("src/practice/task3_2/text.txt");
+            int s = 0;
+            String word = "";
+            List<String> names = new ArrayList<>();
+            while ((s = reader.read()) != -1) {
+                word += (char) s;
+            }
+            if (word != null) {
+                String[] split = word.split(" ");
+                names = new ArrayList<>(List.of(split));
+            }
+
+            for (String name : names) {
+                System.out.print(name + " ");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
